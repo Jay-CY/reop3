a54d218b846ed4045e366aed7f6844623b752f6e
CYJ <824080816@qq.com>
2020-09-15 10:59:44 +0800

    把本地工程添加到Git

---
+++ git-test/src/com/cyj/git/GitTest.java
@@ -0,0 +1,12 @@
+package com.cyj.git;
+
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git project");
+        System.out.println("hello git project");
+        System.out.println("hello git project");
+        System.out.println("hello git project");
+        System.out.println("hello git project");
+        System.out.println("hello git project");
+    }
+}
